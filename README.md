a6617afc9a6177c5a6d7c62afa451e3591be029b
maitha1-nule <nuleahmed6@gmail.com>
2024-05-12 00:32:34 +0300

    Today i started on vue js. was able to create a small todo item, which a user can use to add more items to his item list

---
+++ README.md
@@ -0,0 +1,14 @@
+<center>Vue js crashcourse</center>
+
+On day 61 of #100DaysOfALXSE
+Today i continued learning of Vue by learning:
+========================================
+===> Conditional Rendering
+This is showing content based on a user requirement like loging in.
+In order to do this we use a v-if
+An altenative to v-if is v-show
+v-show is best because it enables the elements to be displayed in a block manner. 
+If not shown the elements have a display property of none.
+
+In this session, I've created a list of items which can only be displayed when a button is clicked
+else they are not shown.
--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 //creating a view app
 const app = Vue.createApp({
-    //data, function
-    //function that returns an object
     data() {
         return {
-            title:'This is a Vue 3 app',
-            name:'Ahmed',
-            age:25
-        }
-    },
+                heading: 'This are my Todo Items for the day',
+                todo: ['Learn Vue,js', 'Finish up on Digital Marketing Course', 'Finish up on Cyber Security Course', 'Finish up on my pending Projects', 'Listen to Music'],
+
+                showHeading: true,
+                showInput: true,
+                newItem: '',
+
+            }
+        },
     methods: {
-        changeTitle(name, miaka) {
-           // this.title = 'software engineers in progress',
-            //this.age = 50
-            this.title = name,
-            this.age = miaka
+        //this function updates the number of items in the leading
+        updateHeading() {
+            if (this.showHeading) {
+                this.heading = 'Updated Todo Items for the day',
+                this.todo.push('Add more tasks to the list')
+            }else {
+                this.heading = 'Reset Todo items',
+                this.todo = ['Reset the list of the List']
+            }
+            this.showHeading = !this.showHeading,
+            this.showInput = true
+            },
+            resetHeading() {
+                this.heading = 'This is my Todo Items for the day',
+                this.todo = [],
+                this.showHeading = true,
+                this.showInput = false
+        },
+        addItem() {
+            if (this.newItem.trim() !== '') {
+                this.todo.push(this.newItem),
+                this.newItem = ''
+            }
         }
     }
 })
