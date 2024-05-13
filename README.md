f7559e6bba3ee21d3c805e3084fe7e13427d1a57
maitha1-nule <nuleahmed6@gmail.com>
2024-05-13 23:22:30 +0300

    Today i did somthing diffrent, continued working with vue,\ today i dived deep into objects and arrays by using for loops, then implemented this on the todo add so as to undesttand everything better. \ aslo dicided to work with diffrent branches for better collaboration on tasks and projects

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ If not shown the elements have a display property of none.
 
 In this session, I've created a list of items which can only be displayed when a button is clicked
 else they are not shown.
+
+In day 62 0f #100daysofALXSE
+Today i dived into outputting lists using vue js, using the v-for, this is very useful because in most situations we need to output lists so that we can be able to hsow alot of data. In this case: 
+                                                        ==> I will use arrays and object for more info see the code.
+
+Also learned about dynamic binding
+                                
--- app.js
+++ app.js
@@ -2,41 +2,21 @@
 const app = Vue.createApp({
     data() {
         return {
-                heading: 'This are my Todo Items for the day',
-                todo: ['Learn Vue,js', 'Finish up on Digital Marketing Course', 'Finish up on Cyber Security Course', 'Finish up on my pending Projects', 'Listen to Music'],
-
-                showHeading: true,
-                showInput: true,
-                newItem: '',
-
-            }
-        },
-    methods: {
-        //this function updates the number of items in the leading
-        updateHeading() {
-            if (this.showHeading) {
-                this.heading = 'Updated Todo Items for the day',
-                this.todo.push('Add more tasks to the list')
-            }else {
-                this.heading = 'Reset Todo items',
-                this.todo = ['Reset the list of the List']
-            }
-            this.showHeading = !this.showHeading,
-            this.showInput = true
-            },
-            resetHeading() {
-                this.heading = 'This is my Todo Items for the day',
-                this.todo = [],
-                this.showHeading = true,
-                this.showInput = false
-        },
-        addItem() {
-            if (this.newItem.trim() !== '') {
-                this.todo.push(this.newItem),
-                this.newItem = ''
-            }
-        }
+            showlanguages: true,
+        title: 'Best programming languages for beginers',
+        languages : [
+            { language: 'HTML', reason: 'The syntax is more of a markup language' },
+            { language:'Python', reason:'Is easy to learn because of the syntax' },
+            { language:'JavaScript' , reason:'Is the most popular language for web development'},
+            { language: 'Java',  reason: 'Is a good language for beginners to learn' },
+            { language: 'C', reason:'Is a good language for beginners to learn because it lays foundation for all other languages'} ,
+        ]
     }
+},
+        methods: {
+            toogleButton() {
+                this.showlanguages = !this.showlanguages
+            },
+        }
 })
-
 app.mount('#app')
